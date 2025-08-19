9df8873ce81f27399121f9b61173416d6b7cfe26
abhix86 <uchihamadarat99@gmail.com>
2025-08-19 08:53:49 +0530

    fixed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
-// const port = 3000;
-import { PORT } from './config/env.js';
 
-import cors from "cors";
+
+import 'dotenv/config'; 
+
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
@@ -10,14 +10,25 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase  from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
-
+import cors  from 'cors'
 const app = express();
 
+
+
+import { PORT } from './config/env.js';
+
+
+
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(express.json())
 app.use(express.urlencoded( { extended: false } ));
 app.use(cookieParser())
+
 
 
 
@@ -34,8 +45,10 @@ app.get('/', (req, res) => {
     res.send("Seems it works")
 });
 
-app.listen(PORT, async () => {
-    console.log(`Server is running: http://localhost:${PORT}`)
+
+const port = process.env.PORT || PORT || 5500;
+app.listen(port, async () => {
+    console.log(`Server is running http:://localhost:${port}`)
 
     await connectToDatabase()
 });
