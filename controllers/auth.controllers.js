3417e2e9b0156a850d4f0b2e32a2352ad61be444
abhix86 <uchihamadarat99@gmail.com>
2025-08-19 09:06:16 +0530

    tch, pain

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,56 +1,42 @@
-import mongoose from "mongoose"
-import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
-import { db } from "../db.js";
+import connectToDatabase from '../../../database/mongodb.js';
+import User from '../../../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { JWT_EXPIRES_IN, JWT_SECRET } from '../../../config/env.js';
+import { json } from 'micro';
 
-export const signUp = async (req, res, next) => {
-    const session = await mongoose.startSession();
-    session.startTransaction();
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 
-    try {
-        const { name, email, password } = req.body;
+  await connectToDatabase();
 
-        const existingUser = await User.findOne({ email });
+  try {
+    const body = await json(req);
+    const { name, email, password } = body;
 
-        if(existingUser) {
-            const error = new Error('User already exists');
-            error.statusCode = 409;
-            throw error;
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        const newUsers = await User.create([{ name, email, password: hashedPassword }], { session });
-
-        const userObj = newUsers
-        
-
-        const token = jwt.sign( { userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN } )
-
-        await session.commitTransaction();
-        session.endSession();
-
-
-        res.status(201).json({
-            success: true,
-            message: 'User created successfully',
-            data: {
-                token,
-                user: userObj[0]
-                
-            }
-        })
-        db.length = 0;         // clear old data if needed
-        db.push(...userObj);
-    } catch (error) {
-        await session.abortTransaction();
-        session.endSession();
-        next(error);
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ name, email, password: hashedPassword });
+
+    const token = jwt.sign({ userId: newUser._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      data: {
+        token,
+        user: newUser
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 export const signIn = async (req, res, next) => {}
