7048718059449b356a29f0c8448362fd75e56e31
abhix86 <uchihamadarat99@gmail.com>
2025-08-19 09:18:09 +0530

    change in deployement

--- app.js
+++ app.js
@@ -1,56 +1,47 @@
 import express from 'express';
-
-
-import 'dotenv/config'; 
-
+import 'dotenv/config';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
-import connectToDatabase  from './database/mongodb.js';
+import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
-import cookieParser from 'cookie-parser';
-import cors  from 'cors'
-const app = express();
-
-
 
 import { PORT } from './config/env.js';
 
+const app = express();
+
 
 app.use(cors({
-    origin: 'https://signupbackend.vercel.app/', // frontend URL
+    origin: '*', 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
-app.use(express.json())
-app.use(express.urlencoded( { extended: false } ));
-app.use(cookieParser())
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
 
 app.use(errorMiddleware);
 
-/* import cors from 'cors';
-app.use(cors());
- */
+
 app.get('/', (req, res) => {
-    res.send("Seems it works")
+    res.send("Well, this works!");
 });
 
 
 const port = process.env.PORT || PORT || 5500;
 app.listen(port, async () => {
-    console.log(`Server is running http:://localhost:${port}`)
-
-    await connectToDatabase()
+    console.log(`Server is running on http://localhost:${port}`);
+    await connectToDatabase();
 });
 
 export default app;
-
--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,20 +1,15 @@
-import connectToDatabase from '../../../../database/mongodb.js';
-import User from '../../../../models/user.model.js';
+import connectToDatabase from '../database/mongodb.js';
+import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { JWT_EXPIRES_IN, JWT_SECRET } from '../../../../config/env.js';
-import { json } from 'micro';
+import { JWT_EXPIRES_IN, JWT_SECRET } from '../config/env.js';
+import { db } from '../db.js';
 
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
-
-  await connectToDatabase();
-
+export const signUp = async (req, res, next) => {
   try {
-    const body = await json(req);
-    const { name, email, password } = body;
+    await connectToDatabase();
+
+    const { name, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -26,6 +21,9 @@ export default async function handler(req, res) {
 
     const token = jwt.sign({ userId: newUser._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
+    // Save in in-memory db for testing
+    db.push(newUser);
+
     res.status(201).json({
       success: true,
       message: 'User created successfully',
@@ -35,9 +33,9 @@ export default async function handler(req, res) {
       }
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
-}
+};
 
 export const signIn = async (req, res, next) => {}
 
