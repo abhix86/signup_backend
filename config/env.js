fb98ab971549ab774b87389c0fc6f14b075b7d9b
abhix86 <uchihamadarat99@gmail.com>
2025-08-19 09:25:10 +0530

    added logic for render .env

--- config/env.js
+++ config/env.js
@@ -1,11 +1,27 @@
 import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+if (process.env.NODE_ENV !== 'production') {
+  const envPath = path.join(__dirname, `.env.${process.env.NODE_ENV || 'development'}.local`);
+  config({ path: envPath });
+}
 
 
 export const {
-     PORT,
-    NODE_ENV,
-    DB_URI,
-    JWT_EXPIRES_IN,
-    JWT_SECRET
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_EXPIRES_IN,
+  JWT_SECRET
 } = process.env;
+
+
+if (!DB_URI) {
+  throw new Error('MONGODB_URI missing in environment variables!');
+}
